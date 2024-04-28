b2d93af9d1ae7087eeccafb53037a2f4775ecd10
Normich ITOUA <nitoua.dev@gmail.com>
2024-04-28 12:56:59 +0100

    Improve Bubble Sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,14 +10,9 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int swapped;
-
-	if (size < 2)
-		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -27,10 +22,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				print_array(array, size);
-				swapped = 1;
 			}
 		}
-		if (!swapped)
-			return;
 	}
 }
